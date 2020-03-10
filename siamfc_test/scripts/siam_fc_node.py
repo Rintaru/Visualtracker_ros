7f5be085deb6c9a5306ec6e36c3064dfd48145a8
Daniel Ruiz <druiz@our.ecu.edu.au>
2020-03-10 14:07:36 +0800

    update

--- siamfc_test/scripts/siam_fc_node.py
+++ siamfc_test/scripts/siam_fc_node.py
@@ -1,4 +1,4 @@
-#!/home/sekiro/final_year_ws/src/py2.7env/bin/python2.7
+#!/home/sekiro/final_year_ws/venv/bin/python3
 
 from __future__ import absolute_import
 from __future__ import division
@@ -15,14 +15,11 @@ import sys
 from glob import glob
 
 import tensorflow as tf
-from sacred import Experiment
 
 CURRENT_DIR = osp.dirname(__file__)
-sys.path.append(osp.join(CURRENT_DIR, 'depend/..'))
+sys.path.append(osp.join(CURRENT_DIR, 'src/..'))
 
-from inference import inference_wrapper
+from SiamFC_tf import inference
 from inference.tracker import Tracker
 from utils.infer_utils import Rectangle
 from utils.misc_utils import auto_select_gpu, mkdir_p, sort_nicely, load_cfgs
-
-ex = Experiment()
