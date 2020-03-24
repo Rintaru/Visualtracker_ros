dcca784e271304b7cbd64016f457bba706e07c18
Daniel Ruiz <druiz@our.ecu.edu.au>
2020-03-24 14:10:48 +0800

    Created node to initialise siamfc

---
+++ siamfc_test/scripts/siam_fc_initialiser.py
@@ -0,0 +1,81 @@
+#!/home/sekiro/final_year_ws/py2.7env/bin/python
+r"""This node outputs the image that is used to initialise the visual tracker."""
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+#standard ros imports
+import roslib
+import rospy
+import rospkg
+#ros msgs to be used
+from sensor_msgs.msg import Image
+#ros service to be used
+from siamfc_test.srv import img_bb_srv, img_bb_srvResponse, img_bb_srvRequest
+#imports for initialising the image
+import cv2
+from cv_bridge import CvBridge
+import numpy as np
+
+class tracking_algorithim():
+    def __init__(self):
+        self.bridge = CvBridge()
+        rospy.init_node('siamfc_initialiser',anonymous=False)
+        #initialise subscriber to cv_camera_node
+        rospy.Subscriber('/cv_camera/image_raw',Image,self.subscriber_callback, queue_size=2)
+    
+    def subscriber_callback(self,image):
+        #convert received image from sensor_msgs/Image to cv::mat
+        self.cv_tgt_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr16')
+        self.cv_tgt_image = self.preprocess(self.cv_tgt_image)
+        #make the sensor_msgs/Image version of tgt_image accesible
+        self.ros_tgt_image = image
+
+
+
+        ##uncomment for debugging purposes
+        #cv2.imshow('camera',self.cv_tgt_image)
+        #print(type(self.cv_tgt_image))
+        #print('\n')
+        #print(self.cv_tgt_image.shape)
+        #cv2.waitKey(1)
+    
+    def preprocess(self,img):
+        res = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return res
+    def postprocess(self,img):
+        res = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        return res
+
+def call_service(image,bbox):
+    try:
+        s=rospy.ServiceProxy('init_img',img_bb_srv)
+        s.call(img=image,bb=bbox)
+        print('ended')
+    except rospy.ServiceException, e:
+        print('Service call failed')
+            
+if __name__ == '__main__':
+    track=tracking_algorithim()
+    print('waiting for init_img service')
+    rospy.wait_for_service('init_img')
+    print('init_img service has been advertised \ngrabbing an initial image')
+
+    while not rospy.is_shutdown():
+        try:
+            cv2.imshow('press o to select initial frame', track.postprocess(track.cv_tgt_image))
+            selected_image=[track.cv_tgt_image, track.ros_tgt_image]
+            if cv2.waitKey(50) & 0xFF == ord('o'):
+                boundingbox = cv2.selectROI(track.postprocess(selected_image[0]))
+                print('bounding box',boundingbox)
+                break
+        except AttributeError:
+            #An attribute error occurs when calling track.cv_tgt_image before the subscriber_callback is called.
+            #Cause is cv_tgt_image attribute is created inside subscriber_callback
+            #This exception is used to give subscriber_callback time to run
+            pass
+    
+    call_service(selected_image[1],boundingbox)
+
+    cv2.destroyAllWindows()
+    
+    rospy.spin()
--- siamfc_test/scripts/siam_fc_node.py
+++ siamfc_test/scripts/siam_fc_node.py
@@ -4,7 +4,7 @@
 # Copyright © 2017 bily     Huazhong University of Science and Technology
 #
 # Distributed under terms of the MIT license.
-
+r"""Visual tracking node."""
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
@@ -16,7 +16,7 @@ import rospkg
 from geometry_msgs.msg import Vector3Stamped 
 from sensor_msgs.msg import Image
 #ros service to be used
-from siamfc_test.srv import img_bb_srv
+from siamfc_test.srv import img_bb_srv, img_bb_srvResponse, img_bb_srvRequest
 #imports for visual tracking
 import cv2
 from cv_bridge import CvBridge
@@ -25,32 +25,35 @@ from siam_fc.SiameseTracker import SiameseTracker
 class tracking_algorithim():
     def __init__(self):
         self.bridge = CvBridge()
+        
+        #load model used by visual tracker
+        rospackage = rospkg.RosPack()
+        model_path = rospackage.get_path('siamfc_test')+'/scripts/siam_fc/Logs/SiamFC/track_model_checkpoints/SiamFC-3s-color-pretrained'
+        self.tracker = SiameseTracker(debug=0, checkpoint=model_path)
+        
         #initialise node
         rospy.init_node('siamfc_node',anonymous=False)
+        
         #initialise publisher
         self.tgt_heading_pub = rospy.Publisher('tgt_heading',Vector3Stamped,queue_size=10)
         self.PubRate = rospy.Rate(10)#defining the rate at which we publish target headings, 10hz
         self.tgt_heading=Vector3Stamped()#this was defined here so that it would only be initialized once to avoid memory issues
-        #initialise subscriber to cv_camera_node
-        rospy.Subscriber('/cv_camera/image_raw',Image,self.subscriber_callback, queue_size=2)
-        #initialise service for init_siamfc_node
-        #rospy.Service('init_img',,)
+        
+        self.init_service()
 
-        self.publish()#this needs to be removed after testing
+        self.init_subscriber()
 
-        #loading model and init tracker
-        rospackage = rospkg.RosPack()
-        model_path = rospackage.get_path('siamfc_test')+'/scripts/siam_fc/Logs/SiamFC/track_model_checkpoints/SiamFC-3s-color-pretrained'
-        self.tracker = SiameseTracker(debug=0, checkpoint=model_path)
+
         print("yeet?")
 
+    #function used to publish the tgt's location
     def publish(self):
         self.PubRate.sleep()
         self.tgt_heading.vector.x=10
         self.tgt_heading_pub.publish(self.tgt_heading)
     
     def subscriber_callback(self,image):
-        #convert image from sensor_msgs/Image to cv::mat
+        #convert received image from sensor_msgs/Image to cv::mat
         cv_image = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr16')
         ##uncomment for debugging purposes
         #cv2.imshow('camera',cv_image)
@@ -59,7 +62,10 @@ class tracking_algorithim():
         #print(cv_image.shape)
         #cv2.waitKey(1)
 
+        #actual tracking of the target occurs here
         reported_bbox = self.tracker.track(cv_image)
+
+        #Show the camera image with a box drawn around the target
         cv2.rectangle(cv_image, (int(reported_bbox[0]), int(reported_bbox[1])),
                       (
                           int(reported_bbox[0]) + int(reported_bbox[2]),
@@ -67,7 +73,30 @@ class tracking_algorithim():
                       (0, 0, 255), 2)
 
         cv2.imshow('frame', cv_image)
-        cv2.waitKey(1)
+        cv2.waitKey(3)
+    
+    #image to initialise the tracker is received here.
+    def service_handle(self,init_img):
+        #convert initial image in to cv::mat
+        cv_init_img=self.bridge.imgmsg_to_cv2(init_img.img)
+        self.tracker.set_first_frame(cv_init_img,init_img.bb)
+        #setting this bool to True will allow this node to subscribe to cv_camera
+        self.is_tracker_init=True
+        return img_bb_srvResponse(True)
+
+    #initialise the service that is used to initialise or re-initialise the visual tracker
+    def init_service(self):
+        rospy.Service('init_img',img_bb_srv,self.service_handle)
+        self.is_tracker_init=False
+
+    #initialise subscriber to cv_camera node
+    def init_subscriber(self):
+        print('initialising subscriber')
+        #if the visual tracker has not received an inital image AND node has not been shutdown then block
+        while not self.is_tracker_init and not rospy.is_shutdown():
+            pass
+        #subscribe to cv_camera node
+        rospy.Subscriber('/cv_camera/image_raw',Image,self.subscriber_callback, queue_size=2)
 
 
 
