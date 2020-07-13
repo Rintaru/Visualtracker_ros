f17df91026b5182cff82188c7f9915ce7a509101
Daniel Ruiz <daniel.ruiz@chironix.com>
2020-07-13 10:01:57 +0800

    misc. changes

--- t265_driver/ros_entrypoint.sh
+++ t265_driver/ros_entrypoint.sh
@@ -4,4 +4,6 @@ set -e
 source "/opt/ros/$ROS_DISTRO/setup.bash"
 cd /home/user/leader_follower_ws
 catkin_make
+source /home/user/leader_follower_ws/devel/setup.bash
+roslaunch image_proc_fisheye image_rectification_fisheye.launch
 exec "$@"
--- t265_driver/run_image.sh
+++ t265_driver/run_image.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker kill /t265_driver																												
-sudo docker run --name t265_driver --privileged --gpus all -it --rm --network host t265_driver:test
+sudo docker run --name t265_driver --privileged --gpus all -it --rm --network host -v /dev/:/dev/ t265_driver:test
